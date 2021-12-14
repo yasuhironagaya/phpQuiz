1b0b444fc8d4d4c18fa84dbddb6b5cc2b04f39fd
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2021-12-14 20:14:14 +0900

    id とﾚｽﾎﾟﾝｽﾍｯﾀﾞの変更

--- html/question.php
+++ html/question.php
@@ -2,25 +2,26 @@
 
 require __DIR__.'/../lib/functions.php';
 
-$id = '1';
+$id = escape($_GET['id'] ?? '');
 
 $data = fetchById($id);
 
+if (!$data) {
+  // HTTPレスポンスのヘッダを404にする
+  header('HTTP/1.1 404 Not Found');
 
-$question = nl2br(htmlspecialchars($data[1]));
+  // レスポンスの種類を指定する
+  header('Content-Type: text/html; charset=UTF-8');
+  include __DIR__.'/../template/404.tpl.php';
+  exit(0);
+}
 
+$formattedData = generateFormattedData($data);
 
-$answers = [
-  'A' => htmlspecialchars($data[2]),
-  'B' => htmlspecialchars($data[3]),
-  'C' => htmlspecialchars($data[4]),
-  'D' => htmlspecialchars($data[5]),
-];
-
-
-
-$correctAnswer = htmlspecialchars(strtoupper($data[6]));
+$question = $formattedData['question'];
+$answers = $formattedData['answers'];
+$correctAnswer = $formattedData['correctAnswer'];
 $correctAnswerValue = $answers[$correctAnswer];
-$explanation = nl2br(htmlspecialchars($data[7]));
+$explanation = $formattedData['explanation'];
 
 include __DIR__.'/../template/question.tpl.php';
---
+++ template/404.tpl.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="./questions.js" defer></script>
+    <title>問題 | Quiz</title>
+  </head>
+  <body>
+    <div id="main">
+    <h1>自己紹介Quiz</h1>
+      問題は見つかりませんでした
+      <div class="section">
+        <a href="./../question.php">戻る</a>
+      </div>
+    </div>
+  </body>
+</html>
