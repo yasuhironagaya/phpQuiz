4e56631a93eccee3f65dd7663407cff879145f70
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2021-12-14 21:20:27 +0900

    form POST

---
+++ html/answer.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__.'/../lib/functions.php';
+
+$id = $_POST['id'] ?? '';
+$selectedAnswer = $_POST['selectedAnswer'] ?? '';
+
+$data = fetchById($id);
+
+if (!$data) {
+  // HTTPレスポンスのヘッダを404にする
+  header('HTTP/1.1 404 Not Found');
+
+  // レスポンスの種類を指定する
+  header('Content-Type: text/html; charset=UTF-8');
+  include __DIR__.'/../template/404.tpl.php';
+  exit(0);
+}
+
+$formattedData = generateFormattedData($data);
+
+$correctAnswer = $formattedData['correctAnswer'];
+$correctAnswerValue = $formattedData['answers'][$correctAnswer];
+$explanation = $formattedData['explanation'];
+
+$result = $selectedAnswer === $correctAnswer;
+
+var_dump($result);
+
+
+echo $correctAnswer;
+echo $correctAnswerValue;
+echo $explanation;
